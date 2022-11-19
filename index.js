94fa3991c70effe8ed1eff99f013ec8a747dcecf
PaulDev97 <paul.devc@gmail.com>
2022-11-19 17:08:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import { catalog } from "./store.js";
+const games = catalog;
+const renderContainer = document.querySelector('.games_container')
+
+console.log(games)
+
+const createElement = games => ` 
+<div class="card_game">
+
+<div class="game_img">
+  <img src="${games.img}" alt="">
+</div>
+<div class="data_game">
+  <div class="name_game">
+    <h3>${games.game}</h3>
+    <span>4.5</span>
+  </div>
+  <div class="category">
+    <span>${games.gener}</span>
+  </div>
+  <div class="price">
+    <div>
+      <span>$ ${games.price}</span>
+      <button>Agregar <i class="fa-solid fa-cart-shopping"></i></button>  
+    </div>
+  </div>
+</div>
+</div> 
+`;
+
+
+
+const paintElement = game => {
+  renderContainer.innerHTML = game.map((item) => createElement(item)).join('');
+}
+
+
+
+const init = () => {
+
+  window.addEventListener('DOMContentLoaded',() => {
+    paintElement(games)
+  })
+}
+
+init() 
---
+++ store.js
@@ -0,0 +1,40 @@
+export const catalog = [
+  {
+    gener:"Acción y aventura",
+    game:"God of War: Ragnarok",
+    price:200,
+    img: "img/god.jpg"
+  },
+  {
+    gener:"Acción y aventura",
+    game:"The Last of Us Part II",
+    price:100,
+    img: "img/last.jpg"
+
+  },
+  {
+    gener:"Acción y aventura",
+    game:"Spider-Man Miles Morales",
+    price:200,
+    img: "img/spiderman.jpg"
+  },
+  {
+    gener:"Acción y aventura",
+    game:"Resident Evil 2 Remake",
+    price:300,
+    img: "img/re.jpg"
+  },
+  {
+    gener:"Acción y aventura",
+    game:"Sekiro: Shadows Die Twice",
+    price:100,
+    img: "img/sekiro.jpg"
+  },
+ 
+  {
+    gener:"Acción y aventura",
+    game:"Ghost of Tsushima",
+    price:200,
+    img: "img/ghost.jpg"
+  },
+]
